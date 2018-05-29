7924b88338517041396c7f508d96523609de4d47
Peter-Kubik <32175402+Peter-Kubik@users.noreply.github.com>
2018-05-29 21:28:59 +0100

    Update gillespie_function.R

--- gillespie_function.R
+++ gillespie_function.R
@@ -80,8 +80,8 @@ gillespie <- function(x1, x2, iteration, lambda1, beta1,
     #   message("The random walk has reached stationarity at iteration: ", i)
     # }
   }
-  
-  time_duration <- tail(time_keeper, iteration-1) - head(time_keeper, iteration-1)
+  time_duration[1] <- time_keeper[1]
+  time_duration[2:iteration] <- tail(time_keeper, iteration-1) - head(time_keeper, iteration-1)
   x1_average <- weighted.mean(x=tail(x1_storage, check_interval), w=tail(time_duration, check_interval))
   x2_average <- weighted.mean(x=tail(x2_storage, check_interval), w=tail(time_duration, check_interval))
   
